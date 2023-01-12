03abbc1bfd82d31a74d404f80f123bed62eb03b8
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-01-12 15:53:51 +0800

    Add files via upload

---
+++ inc/console.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/env python
+# coding=utf-8
+
+from inc import output,run
+import sys
+
+# 控制台-参数处理和程序调用
+def filereverse_console(args):
+    if args.baseout:
+        run.baseout(args.baseout)
+    if args.hhex:
+        run.hhex(args.hhex)
+    if args.unhex:
+        run.unhex(args.unhex)
+    if args.inversion:
+        run.inversion(args.inversion)
+    if args.reverse:
+        run.reverse(args.reverse)
+    if args.reversal:
+        run.reversal(args.reversal)
+    else:
+        output.usage()
+        sys.exit()
